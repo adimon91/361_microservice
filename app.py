007dde6863d1af9dca6d2f6a866f6f44c1ced53f
Angela <angeladimon91@gmail.com>
2021-11-15 09:18:44 -1000

    Initial commit

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, url_for, request, redirect
+import requests
+
+app = Flask(__name__)
+
+@app.route('/<ticker>', methods=['GET'])
+def index(ticker):
+
+    url = "https://yh-finance.p.rapidapi.com/stock/v2/get-profile"
+
+    querystring = {"symbol":ticker,"region":"US"}
+
+    headers = {
+        'x-rapidapi-host': "yh-finance.p.rapidapi.com",
+        'x-rapidapi-key': "3881371422mshef5287c1e084505p1b5e4cjsn4e06a31bae67"
+        }
+
+    response = requests.request("GET", url, headers=headers, params=querystring)
+
+
+    res = response.json()
+
+    data = {
+        'description': res['assetProfile']['longBusinessSummary'],
+        'price': res['price']['regularMarketPrice']['fmt'],
+        'name': res['price']['shortName']
+        }
+    return data
+
+if __name__ == "__main__":
+    app.run(debug=True)
